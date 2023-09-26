9cacd4251e489b01c385767d17d3a0cc34c43951
ayushi282 <ayushimukherjee0@gmail.com>
2023-09-26 20:29:18 +0530

    html+css

--- src/App.js
+++ src/App.js
@@ -3,21 +3,102 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className='back'>
+   <nav className='navbar'>
+    <img className='imgnavbar' src='/images/n-21.png'></img>
+   </nav>
+
+   <p className='para'>
+   <span className='red'>Unlock Efficiency:</span>
+   <span className='blue'>Elevate Your Business with Cloud Transformation</span>
+   </p>
+   <p className='paar1'>
+   Discover how our expert cloud transformation services can optimize your operations, reduce costs, and enhance scalability, empowering small and mid-size companies to thrive in today's digital landscape.
+   </p>
+   <button className='free'>Book A Free Consultation Today!</button>
+   <div className='box1'>
+       <svg xmlns="http://www.w3.org/2000/svg" width="276" height="196" viewBox="0 0 446 386" fill="none">
+  <g filter="url(#filter0_d_21_467)">
+    <path d="M94 100C94 94.4772 98.4772 90 104 90H342C347.523 90 352 94.4772 352 100V278C352 283.523 347.523 288 342 288H104C98.4772 288 94 283.523 94 278V100Z" fill="white" fill-opacity="0.8" shape-rendering="crispEdges"/>
+    <path d="M94.5 100C94.5 94.7533 98.7533 90.5 104 90.5H342C347.247 90.5 351.5 94.7533 351.5 100V278C351.5 283.247 347.247 287.5 342 287.5H104C98.7533 287.5 94.5 283.247 94.5 278V100Z" stroke="black" stroke-opacity="0.1" shape-rendering="crispEdges"/>
+  </g>
+  <defs>
+    <filter id="filter0_d_21_467" x="0" y="0" width="446" height="386" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+      <feOffset dy="4"/>
+      <feGaussianBlur stdDeviation="47"/>
+      <feComposite in2="hardAlpha" operator="out"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/>
+      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_21_467"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_21_467" result="shape"/>
+    </filter>
+  </defs>
+</svg>
+<div className='expert'>Expert Advice</div>
+<div className='tap'>Tap into the knowledge of our experienced consultants with over 30 years of collective IT Experience.</div>
+   <div className='one'>1</div>
+   </div>
+   <div className='box2'>
+   <svg xmlns="http://www.w3.org/2000/svg" width="276" height="196" viewBox="0 0 446 386" fill="none">
+  <g filter="url(#filter0_d_21_467)">
+    <path d="M94 100C94 94.4772 98.4772 90 104 90H342C347.523 90 352 94.4772 352 100V278C352 283.523 347.523 288 342 288H104C98.4772 288 94 283.523 94 278V100Z" fill="white" fill-opacity="0.8" shape-rendering="crispEdges"/>
+    <path d="M94.5 100C94.5 94.7533 98.7533 90.5 104 90.5H342C347.247 90.5 351.5 94.7533 351.5 100V278C351.5 283.247 347.247 287.5 342 287.5H104C98.7533 287.5 94.5 283.247 94.5 278V100Z" stroke="black" stroke-opacity="0.1" shape-rendering="crispEdges"/>
+  </g>
+  <defs>
+    <filter id="filter0_d_21_467" x="0" y="0" width="446" height="386" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+      <feOffset dy="4"/>
+      <feGaussianBlur stdDeviation="47"/>
+      <feComposite in2="hardAlpha" operator="out"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/>
+      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_21_467"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_21_467" result="shape"/>
+    </filter>
+  </defs>
+</svg>
+<div className='expert'>Tailored Solutions</div>
+<div className='tap'>Get customized strategies for your company’s specific goals.</div>
+<div className='one'>2</div>
+   </div>
+   <div className='box3'>
+   <svg xmlns="http://www.w3.org/2000/svg" width="276" height="196" viewBox="0 0 446 386" fill="none">
+  <g filter="url(#filter0_d_21_479)">
+    <path d="M94 100C94 94.4772 98.4772 90 104 90H342C347.523 90 352 94.4772 352 100V278C352 283.523 347.523 288 342 288H104C98.4772 288 94 283.523 94 278V100Z" fill="white" fill-opacity="0.8" shape-rendering="crispEdges"/>
+    <path d="M94.5 100C94.5 94.7533 98.7533 90.5 104 90.5H342C347.247 90.5 351.5 94.7533 351.5 100V278C351.5 283.247 347.247 287.5 342 287.5H104C98.7533 287.5 94.5 283.247 94.5 278V100Z" stroke="black" stroke-opacity="0.1" shape-rendering="crispEdges"/>
+  </g>
+  <defs>
+    <filter id="filter0_d_21_479" x="0" y="0" width="446" height="386" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+      <feOffset dy="4"/>
+      <feGaussianBlur stdDeviation="47"/>
+      <feComposite in2="hardAlpha" operator="out"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/>
+      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_21_479"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_21_479" result="shape"/>
+    </filter>
+  </defs>
+</svg>
+<div className='expert'>Expert Advice</div>
+<div className='tap'>Limited Offer: Act now to secure your complimentary consultation slot.</div>
+<div className='one'>3</div>
+   </div>
+   <div className='cal'>
+<div className='cal1'>
+</div>
+<span className='book'>Book your Consultation</span>
+<input className='name' placeholder='Your Name'></input>
+<input className='email' placeholder='Email Address'></input>
+<img className='cross' src='/images/image-2.png'></img>
+<button className='submit'>Submit</button>
+   </div>
+    </div>
+    <div>
+      
+    </div>
     </div>
   );
 }
